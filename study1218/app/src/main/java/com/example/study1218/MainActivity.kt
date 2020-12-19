554a9df41a0e871c8af7f0c7065daa87751658f4
heejeong116 <0120_h@naver.com>
2020-12-19 22:12:53 +0900

    12월 19일 커밋

--- study1218/app/src/main/java/com/example/study1218/MainActivity.kt
+++ study1218/app/src/main/java/com/example/study1218/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.study1218
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +12,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+
 }
