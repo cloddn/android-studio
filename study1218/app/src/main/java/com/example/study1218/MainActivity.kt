71f8d9d9cd92dade62dd4d7f20726c6e32f3a8dc
heejeong116 <0120_h@naver.com>
2020-12-20 23:52:34 +0900

    12월 20일 커밋

--- study1218/app/src/main/java/com/example/study1218/MainActivity.kt
+++ study1218/app/src/main/java/com/example/study1218/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.study1218
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
-import android.widget.Toast
+import androidx.annotation.MainThread
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +14,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn1: Button =findViewById(R.id.btn1)
+
+         btn1.setOnClickListener (new View.OnsetOnClickListener){
+            public void OnClick(View v)
+            {
+                 Intent intent = new Intent(MainActivity.this, SubActivity.class)
+                         startActivity(intent)
+             }
+
+
+     }
+
 
     }
 
