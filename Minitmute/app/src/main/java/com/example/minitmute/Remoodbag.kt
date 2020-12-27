a516f59d6954e239a96efa74ed39d253f4468bae
heejeong116 <0120_h@naver.com>
2020-12-28 01:09:57 +0900

    12월 27일 커밋

---
+++ Minitmute/app/src/main/java/com/example/minitmute/Remoodbag.kt
@@ -0,0 +1,11 @@
+package com.example.minitmute
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Remoodbag : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_remoodbag)
+    }
+}
