d84e2aae000911bce173936d013d8b1fedf040eb
heejeong116 <0120_h@naver.com>
2021-01-19 23:20:31 +0900

    1월 19일 커밋

---
+++ IDMemoList/app/src/main/java/com/example/idmemolist/DataListEdit.kt
@@ -0,0 +1,11 @@
+package com.example.idmemolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DataListEdit : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_data_list_edit)
+    }
+}
